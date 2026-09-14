485b626e92fa4d11de7a91cfc9b6f8001593d6be
Tancredi-Paul Grozav <paul@grozav.info>
2026-09-14 22:25:23 +0300

    deb

--- images/openvpn-client/build_context/fs/root/setup.sh
+++ images/openvpn-client/build_context/fs/root/setup.sh
@@ -1,30 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 # ============================================================================ #
 # Author: Tancredi-Paul Grozav <paul@grozav.info>
 # ============================================================================ #
 set -x &&
-date &&
 echo "Building image ..." &&
-
-apk add \
-  ` # prevents Alpine from saving local package index caches to disk, ` \
-  ` # keeping the layer minimal. ` \
-  --no-cache \
+export DEBIAN_FRONTEND=noninteractive &&
+apt-get install \
   ` # Connect to remote networks ` \
   openvpn \
   ` # OTP generator for logging in with 2FA ` \
-  oath-toolkit-oathtool \
+  oathtool \
   ` # Forward tcp ports from local to VPN destination ` \
   socat \
   ` # Interact over std in and out with programs ` \
   expect \
   ` # ping to debug vpn access ` \
-  ` # iputils-ping ` \
+  iputils-ping \
   ` # curl to debug vpn access ` \
-  ` # curl ` \
+  curl \
   &&
-# apt-get clean &&
-# ​rm -rf /var/lib/apt/lists/* &&
+apt-get clean &&
+​rm -rf /var/lib/apt/lists/* &&
+
 echo "Done installing packages" &&
 set +x &&
 true
