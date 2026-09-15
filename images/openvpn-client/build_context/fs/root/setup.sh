c618627acaa3b96ec6466e564221aabb1f983bb7
Tancredi-Paul Grozav <paul@grozav.info>
2026-09-15 08:54:57 +0300

    switch to alpine

--- images/openvpn-client/build_context/fs/root/setup.sh
+++ images/openvpn-client/build_context/fs/root/setup.sh
@@ -1,28 +1,33 @@
-#!/bin/bash
+#!/bin/sh
 # ============================================================================ #
 # Author: Tancredi-Paul Grozav <paul@grozav.info>
 # ============================================================================ #
 set -x &&
 echo "Building image ..." &&
-export DEBIAN_FRONTEND=noninteractive &&
-apt-get update &&
-apt-get install -y \
+# export DEBIAN_FRONTEND=noninteractive &&
+# apt-get update &&
+# apt-get install -y \
+# apt-get clean &&
+# rm -rf /var/lib/apt/lists/* &&
+apk add \
+  ` # Downloads the index temporarily to memory and deletes it immediately after
+  ` # installation finishes. This is to reduce the image size ` \
+  --no-cache
   ` # Connect to remote networks ` \
   openvpn \
   ` # OTP generator for logging in with 2FA ` \
-  oathtool \
+  oath-toolkit-oathtool \
   ` # Forward tcp ports from local to VPN destination ` \
   socat \
   ` # Interact over std in and out with programs ` \
   expect \
-  ` # ping to debug vpn access ` \
-  iputils-ping \
+  ` # Needed for the command: script . To simulate a tty for expect cmd ` \
+  util-linux \
+  ` # ping to debug vpn access. In alpine it exists, only debian needs it ` \
+  ` # iputils-ping ` \
   ` # curl to debug vpn access ` \
   curl \
   &&
-apt-get clean &&
-rm -rf /var/lib/apt/lists/* &&
-
 echo "Done installing packages" &&
 set +x &&
 true
